60b5fce12ed85a3718a24501fc9311be86012f96
Dylan Turner <dylantdmt@gmail.com>
2022-05-18 20:53:02 -0500

    Added ebnf for parsing expressions

--- src/parser.rs
+++ src/parser.rs
@@ -6,5 +6,11 @@
 /*
  * Pseudo-EBNF
  * 
- * <term> := TODO
+ * <number> ::=     [0-9+] ('.' [0-9])
+ * <term> ::=       <number> ('+' | '-') <number> | <number>
+ * <factor> ::=     <term> ('*' | '/' | '%') <term> | <term>
+ * <expr> ::=       <factor> | '(' <expr> ')' | '-' <expr> | '\' <expr> | /[a-z]/ ":=" <expr>
+ * 
+ * <keyword> ::=    '^' | "sqrt" | "sin" | "cos" | "tan" | "asin" | "acos" | "atan" | "j"
+ * <func> ::=       (<keyword> | "log_" /[0-9]+/) '(' <expr> ')'
  */
