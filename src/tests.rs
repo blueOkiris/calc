8aa17d609ba403c8b870a7ee6c44db3453ccc69d
Dylan Turner <dylantdmt@gmail.com>
2022-09-07 15:27:13 -0500

    tests.rs: renamed functions to match changes in parser

--- src/tests.rs
+++ src/tests.rs
@@ -6,34 +6,34 @@
 #[cfg(test)]
 
 use crate::parser::{
-    retrieve_integer,
-    retrieve_number,
-    retrieve_ident
+    parse_integer,
+    parse_number,
+    parse_ident
 };
 
 #[test]
 pub fn parse_ints() {
-    assert!(retrieve_integer("1").is_none());
-    assert!(retrieve_integer("1_").is_some());
-    assert!(retrieve_integer("1.2_").is_none());
-    assert!(retrieve_integer("1_000_000_").is_some());
-    assert!(retrieve_integer("-10_").is_some());
+    assert!(parse_integer("1").is_none());
+    assert!(parse_integer("1_").is_some());
+    assert!(parse_integer("1.2_").is_none());
+    assert!(parse_integer("1_000_000_").is_some());
+    assert!(parse_integer("-10_").is_some());
 }
 
 #[test]
 pub fn parse_floats() {
-    assert!(retrieve_number("1").is_some());
-    assert!(retrieve_number("1.2_").is_some());
-    assert!(retrieve_number("-1.2").is_some());
-    assert!(retrieve_number(".9E-7").is_some());
-    assert!(retrieve_number("6.022E24").is_some());
+    assert!(parse_number("1").is_some());
+    assert!(parse_number("1.2_").is_some());
+    assert!(parse_number("-1.2").is_some());
+    assert!(parse_number(".9E-7").is_some());
+    assert!(parse_number("6.022E24").is_some());
 }
 
 #[test]
 pub fn parse_names() {
-    assert!(retrieve_ident("").is_none());
-    assert!(retrieve_ident("aA_9_d_.a").is_some());
-    assert!(retrieve_ident("9a").is_none());
-    assert!(retrieve_ident("a").is_some());
+    assert!(parse_ident("").is_none());
+    assert!(parse_ident("aA_9_d_.a").is_some());
+    assert!(parse_ident("9a").is_none());
+    assert!(parse_ident("a").is_some());
 }
 
