27366034233a939e5b89753431378eb0e0d67d66
Dylan Turner <dylantdmt@gmail.com>
2022-09-06 19:50:34 -0500

    parser.rs: created and added comment with language EBNF

---
+++ src/parser.rs
@@ -0,0 +1,25 @@
+/*
+ * Author: Dylan Turner
+ * Description: Take string/repl input and turn it into an AST
+ */
+
+/*
+ * EBNF:
+ *
+ * <stmt>           ::= <expr> | <func-def> | <asgn>
+ * <func-def>       ::= '\' <ident> '(' [ <ident> { ',' <ident> } ] ')' '=' <expr>
+ * <asgn>           ::= 'let' <ident> ':=' <expr>
+ * <expr>           ::= <expr> | '(' <expr> ')'
+ * <exp>            ::= <product> | <product> '^' <product>
+ * <product>        ::= <sum> | <sum> ( '*' | '/' ) <sum>
+ * <sum>            ::= <term> | <term> ( '+' | '-' ) <term>
+ * <term>           ::= <ident> | <func-call> | <float> | <int>
+ *                    | 'j' <term> | '-' <term>
+ * <func-call>      ::= <ident> '(' [ <expr> { ',' <expr> } ] ')'
+ * <ident>          ::= /[A-Za-z_]+[A-Za-z_0-9]* /
+ * <float>          ::= /\-?([0-9]+\.)?[0-9]+([Ee]\-?[0-9]+)?/
+ * <int>            ::= /[0-9]+\_/
+ */
+
+
+
