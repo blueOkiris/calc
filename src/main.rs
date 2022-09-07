7ea457806121278b435c4cc5bdf2379bd9cbadc7
Dylan Turner <dylantdmt@gmail.com>
2022-09-07 16:50:21 -0500

    main.rs: added initial if statement for statement vs REPL

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,20 @@
 
 mod parser;
 mod tests;
+mod args;
+
+use parser::parse_stmt;
+
+use crate::args::cli_args;
 
 fn main() {
-    
+    let args = cli_args();
+    if args.is_present("stmts") {
+        let stmt = parse_stmt(args.value_of("stmts").unwrap());
+        println!("Parsed {:?}", stmt.unwrap().token);
+        // TODO: Immediately evaluate
+    } else {
+        // TODO: Enter REPL
+    }
 }
+
