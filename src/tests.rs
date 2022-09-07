c55e0ce63901fda98d7c32c072836ab5d0393f73
Dylan Turner <dylantdmt@gmail.com>
2022-09-07 15:06:16 -0500

    tests.rs: added test of float parsing

--- src/tests.rs
+++ src/tests.rs
@@ -5,7 +5,10 @@
 
 #[cfg(test)]
 
-use crate::parser::retrieve_integer;
+use crate::parser::{
+    retrieve_integer,
+    retrieve_float
+};
 
 #[test]
 pub fn parse_ints() {
@@ -13,5 +16,16 @@ pub fn parse_ints() {
     assert!(retrieve_integer("1_").is_some());
     assert!(retrieve_integer("1.2_").is_none());
     assert!(retrieve_integer("1_000_000_").is_some());
+    assert!(retrieve_integer("-10_").is_some());
 }
+
+#[test]
+pub fn parse_floats() {
+    assert!(retrieve_float("1").is_some());
+    assert!(retrieve_float("1.2_").is_some());
+    assert!(retrieve_float("-1.2").is_some());
+    assert!(retrieve_float(".9E-7").is_some());
+    assert!(retrieve_float("6.022E24").is_some());
+}
+
 
