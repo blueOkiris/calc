87288d42d9fb88a1ac806b8210306085f1df63f9
Dylan Turner <dylantdmt@gmail.com>
2022-09-07 15:05:57 -0500

    parser.rs: added float parsing

--- src/parser.rs
+++ src/parser.rs
@@ -48,14 +48,19 @@ pub struct ParseResult {
 pub fn retrieve_integer(code: &str) -> Option<ParseResult> {
     let mut int_str = String::new();
     let mut i = 0;
+
+    // Check for negative num
     if code.len() > 0 && code.chars().nth(0).unwrap() == '-' {
         int_str.push('-');
         i = 1;
     }
+
+    // Get 0-9+, but allow inner '_' for breaking up big numbers
     while i < code.len() && (
-        char::is_digit(code.chars().nth(i).unwrap(), 10) || code.chars().nth(i).unwrap() == '_'
+        code.chars().nth(i).unwrap().is_digit(10) || code.chars().nth(i).unwrap() == '_'
     ) {
-        if i == 0 && !char::is_digit(code.chars().nth(i).unwrap(), 10) {
+        // No starting with '_'
+        if i == 0 && !code.chars().nth(i).unwrap().is_digit(10) {
             return None;
         }
 
@@ -63,6 +68,7 @@ pub fn retrieve_integer(code: &str) -> Option<ParseResult> {
         i += 1;
     }
 
+    // Finally, make sure it's a number and ends with _ and ship it
     if int_str.len() < 2 || !int_str.ends_with('_') {
         None
     } else {
@@ -70,6 +76,70 @@ pub fn retrieve_integer(code: &str) -> Option<ParseResult> {
             new_start: i,
             token: Token::Integer(int_str.clone())
         })
+    }
+}
+
+// <float> ::= /\-?([0-9]*\.)?[0-9]+([Ee]\-?[0-9]+)?/
+pub fn retrieve_float(code: &str) -> Option<ParseResult> {
+    let mut float_str = String::new();
+    let mut i = 0;
+    let mut found_pt = false;
+
+    // Like int, check for negative
+    if code.len() > 0 && code.chars().nth(0).unwrap() == '-' {
+        float_str.push('-');
+        i = 1;
+    }
+
+    // Get 0-9+ and 0-9+.0-9+
+    while i < code.len() && (
+        code.chars().nth(i).unwrap().is_digit(10)
+            || (code.chars().nth(i).unwrap() == '.' && !found_pt)
+    ) {
+        if code.chars().nth(i).unwrap() == '.' {
+            found_pt = true;
+        }
+
+        float_str.push(code.chars().nth(i).unwrap());
+        i += 1;
+    }
+
+    // Scientific notation
+    if i < code.len() && (
+        code.chars().nth(i).unwrap() == 'E' || code.chars().nth(i).unwrap() == 'e'
+        ) {
+        // Might fail, so don't adjust i and str unless successful
+        let mut j = i + 1;
+        let mut e_str = String::from("E");
+        
+        // Handle negative in size
+        if code.chars().nth(j).unwrap() == '-' {
+            e_str.push('-');
+            j += 1;
+        }
+
+        // Get the 0-9 part
+        let mut num_str = String::new();
+        while j < code.len() && code.chars().nth(j).unwrap().is_digit(10) {
+            num_str.push(code.chars().nth(j).unwrap());
+            j += 1;
+        }
+
+        if num_str.len() > 0 {
+            // Success!
+            i = j;
+            float_str += e_str.as_str();
+            float_str += num_str.as_str();
+        }
+    }
+
+    if float_str.len() > 1 || float_str.chars().nth(0).unwrap() != '-' {
+        Some(ParseResult {
+            new_start: i,
+            token: Token::Number(float_str.clone())
+        })
+    } else {
+        None
     }
 }
 
