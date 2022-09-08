ff7bb37be4366e3a4e40d716da80142a5e51d374
Dylan Turner <dylantdmt@gmail.com>
2022-09-08 15:45:52 -0500

    eval.rs: added the environment manipulating evals. Next is the main eval

--- src/eval.rs
+++ src/eval.rs
@@ -21,9 +21,22 @@ pub struct Var {
     pub lat_int_data: Option<BigInt>
 }
 
+impl Var {
+    pub fn to_string(&self) -> String {
+        String::from("Not implemented")
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Func {
+    pub args: Vec<String>,
+    pub expr: Token
+}
+
+#[derive(Clone, Debug)]
 pub struct Environment {
     pub vars: HashMap<String, Var>,
-    pub funcs: HashMap<String, Token>
+    pub funcs: HashMap<String, Func>
 }
 
 impl Environment {
@@ -36,6 +49,63 @@ impl Environment {
 }
 
 pub fn eval(ast: &Token, env: &mut Environment) -> String {
-    String::from("Not implemented")
+    if let Token::Statement(stmt) = ast {
+        match stmt.as_ref() {
+            Token::FunctionDefinition(name, args, sub_expr) => eval_func_def(
+                name, args, sub_expr, env
+            ), Token::Assignment(name, sub_expr) => eval_asgn(name, sub_expr, env),
+            Token::Expression(sub_ast) => eval_expr(sub_ast, env).to_string(),
+            _ => String::from("Not implemented")
+        }
+    } else {
+        String::from("Not implemented")
+    }
+}
+
+// Add a function for later
+fn eval_func_def(
+        name: &String, args: &Vec<String>, expr: &Token, env: &mut Environment) -> String {
+    let succ = env.funcs.insert(
+        name.clone(),
+        Func {
+            args: args.clone(),
+            expr: expr.clone()
+        }
+    );
+
+    // Newly added
+    if succ.is_none() {
+        format!("Added function '{}' with args {:?} to environment.", name, args)
+    } else {
+        // Updated
+        format!("Updated function '{}'", name)
+    }
+}
+
+// Add/set an identifier
+fn eval_asgn(name: &String, sub_expr: &Token, env: &mut Environment) -> String {
+    let succ = env.vars.insert(name.clone(), eval_expr(sub_expr, env));
+
+    // Newly added
+    if succ.is_none() {
+        format!(
+            "Added var '{}' with value {} to environment.",
+            name, env.vars[&name.clone()].to_string()
+        )
+    } else {
+        // Updated
+        format!("Updated var '{}' to {}", name, env.vars[&name.clone()].to_string())
+    }
+}
+
+// Meat and bones - actually calculate stuff
+fn eval_expr(ast: &Token, env: &Environment) -> Var {
+    Var {
+        ls_data: None,
+        real_int_data: None,
+        lat_int_data: None,
+        real_num_data: None,
+        lat_num_data: None
+    }
 }
 
