b4b398d2179b618ea405bb39612083801040160e
Dylan Turner <dylantdmt@gmail.com>
2022-09-08 17:26:16 -0500

    eval.rs: used new ops to implement product and sum expressions

--- src/eval.rs
+++ src/eval.rs
@@ -41,8 +41,10 @@ pub fn eval(ast: &Token, env: &mut Environment) -> String {
             Token::FunctionDefinition(name, args, sub_expr) => eval_func_def(
                 name, args, sub_expr, env
             ), Token::Assignment(name, sub_expr) => eval_asgn(name, sub_expr, env),
-            Token::Expression(sub_ast) => eval_expr(sub_ast, env).to_string(),
-            _ => String::from("Not implemented")
+            Token::Expression(sub_ast) => match eval_expr(sub_ast, env) {
+                Err(err) => format!("Error: {}", err),
+                Ok(val) => val.to_string()
+            }, _ => String::from("Not implemented")
         }
     } else {
         String::from("Not implemented")
@@ -71,22 +73,27 @@ fn eval_func_def(
 
 // Add/set an identifier
 fn eval_asgn(name: &String, sub_expr: &Token, env: &mut Environment) -> String {
-    let succ = env.vars.insert(name.clone(), eval_expr(sub_expr, env));
+    match eval_expr(sub_expr, env) {
+        Err(err) => format!("Error: {}", err),
+        Ok(val) => {
+            let succ = env.vars.insert(name.clone(), val);
 
-    // Newly added
-    if succ.is_none() {
-        format!(
-            "Added var '{}' with value {} to environment.",
-            name, env.vars[&name.clone()].to_string()
-        )
-    } else {
-        // Updated
-        format!("Updated var '{}' to {}", name, env.vars[&name.clone()].to_string())
+            // Newly added
+            if succ.is_none() {
+                format!(
+                    "Added var '{}' with value {} to environment.",
+                    name, env.vars[&name.clone()].to_string()
+                )
+            } else {
+                // Updated
+                format!("Updated var '{}' to {}", name, env.vars[&name.clone()].to_string())
+            }
+        }
     }
 }
 
 // Meat and bones - actually calculate stuff
-fn eval_expr(ast: &Token, env: &Environment) -> Var {
+fn eval_expr(ast: &Token, env: &Environment) -> Result<Var, String> {
     match ast {
         Token::Expression(un) => eval_expr(un, env),
         Token::UnaryExpression(exp, op) => {
@@ -94,9 +101,21 @@ fn eval_expr(ast: &Token, env: &Environment) -> Var {
                 eval_expr(exp, env)
             } else {
                 match op.clone().unwrap().as_str() {
-                    "j" => eval_expr(exp, env).to_lat(),
-                    "-" => eval_expr(exp, env).to_neg(),
-                    _ => Var::impossible()
+                    "j" => {
+                        let val = eval_expr(exp, env);
+                        if val.is_err() {
+                            val
+                        } else {
+                            Ok(val.unwrap().to_lat())
+                        }
+                    }, "-" => {
+                        let val = eval_expr(exp, env);
+                        if val.is_err() {
+                            val
+                        } else {
+                            Ok(val.unwrap().to_neg())
+                        }
+                    }, _ => Err(String::from("Impossible!"))
                 }
             }
         }, Token::ExponentialExpression(left, right) => {
@@ -104,10 +123,52 @@ fn eval_expr(ast: &Token, env: &Environment) -> Var {
                 eval_expr(left, env)
             } else {
                 let left_val = eval_expr(left, env);
+                if left_val.is_err() {
+                    return left_val;
+                }
                 let right_val = eval_expr(right.clone().unwrap().as_ref(), env);
-                left_val + right_val
+                if right_val.is_err() {
+                    return right_val;
+                }
+                Ok(left_val.unwrap() ^ right_val.unwrap())
             }
-        }, _ => Var::impossible()
+        }, Token::ProductExpression(left, op, right) => {
+            if right.is_none() {
+                eval_expr(left, env)
+            } else {
+                let left_val = eval_expr(left, env);
+                if left_val.is_err() {
+                    return left_val;
+                }
+                let right_val = eval_expr(right.clone().unwrap().as_ref(), env);
+                if right_val.is_err() {
+                    return right_val;
+                }
+                match op.clone().unwrap().as_str() {
+                    "*" => Ok(left_val.unwrap() * right_val.unwrap()),
+                    "/" => Ok(left_val.unwrap() / right_val.unwrap()),
+                    _ => Err(String::from("Impossible!"))
+                }
+            }
+        }, Token::SumExpression(left, op, right) => {
+            if right.is_none() {
+                eval_expr(left, env)
+            } else {
+                let left_val = eval_expr(left, env);
+                if left_val.is_err() {
+                    return left_val;
+                }
+                let right_val = eval_expr(right.clone().unwrap().as_ref(), env);
+                if right_val.is_err() {
+                    return right_val;
+                }
+                match op.clone().unwrap().as_str() {
+                    "+" => Ok(left_val.unwrap() + right_val.unwrap()),
+                    "-" => Ok(left_val.unwrap() - right_val.unwrap()),
+                    _ => Err(String::from("Impossible!"))
+                }
+            }
+        }, _ => Err(String::from("Impossible!"))
     }
 }
 
