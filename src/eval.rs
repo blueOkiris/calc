51ebfe1a23b4c92c7bed59251e69073b3f1c1ed6
Dylan Turner <dylantdmt@gmail.com>
2022-09-08 17:06:00 -0500

    eval.rs: implemented expon expr

--- src/eval.rs
+++ src/eval.rs
@@ -9,110 +9,10 @@ use std::{
     collections::HashMap,
     str::FromStr
 };
-use crate::parser::Token;
-
-// Can be list, complex float, or complex int
-#[derive(Clone, Debug)]
-pub struct Var {
-    pub ls_data: Option<Vec<Var>>,
-    pub real_num_data: Option<BigDecimal>,
-    pub lat_num_data: Option<BigDecimal>,
-    pub real_int_data: Option<BigInt>,
-    pub lat_int_data: Option<BigInt>
-}
-
-// Mainly the base functions 
-impl Var {
-    pub fn to_string(&self) -> String {
-        let mut repr = String::new();
-        if self.ls_data.is_none() {
-            if self.real_num_data.is_some() {
-                repr.push_str(self.real_num_data.clone().unwrap().to_string().as_str());
-            } else if self.real_int_data.is_some() {
-                repr.push_str(self.real_int_data.clone().unwrap().to_string().as_str());
-            }
-            if self.lat_num_data.is_some() {
-                if repr.len() > 0 {
-                    repr.push('+');
-                }
-                repr.push('j');
-                repr.push_str(self.lat_num_data.clone().unwrap().to_string().as_str());
-            } else {
-                if repr.len() > 0 {
-                    repr.push('+');
-                }
-                repr.push('j');
-                repr.push_str(self.lat_int_data.clone().unwrap().to_string().as_str());
-            }
-        } else {
-            repr.push_str("[ ");
-            for var in self.ls_data.clone().unwrap() {
-                repr.push_str(var.to_string().as_str());
-                repr.push(' ');
-            }
-            repr.push(']');
-        }
-        if repr.len() < 1 {
-            repr = String::from("IMPOSSIBLE DATA ACHIEVED")
-        }
-        repr
-    }
-
-    pub fn impossible() -> Self {
-        Self {
-            ls_data: None,
-            real_num_data: None,
-            lat_num_data: None,
-            real_int_data: None,
-            lat_int_data: None
-        }
-    }
-
-    pub fn to_lat(&self) -> Self {
-        let mut new_self = self.clone();
-
-        if new_self.real_num_data.is_some() {
-            new_self.lat_num_data = new_self.real_num_data;
-            new_self.real_num_data = None;
-        } else if new_self.real_int_data.is_some() {
-            new_self.lat_int_data = new_self.real_int_data;
-            new_self.real_int_data = None;
-        } else if new_self.ls_data.is_some() {
-            let mut new_ls = Vec::new();
-            let ls = new_self.ls_data.unwrap();
-            for var in ls {
-                new_ls.push(var.to_lat());
-            }
-            new_self.ls_data = Some(new_ls);
-        }
-        
-        new_self
-    }
-
-    pub fn to_neg(&self) -> Self {
-        let mut new_self = self.clone();
-
-        if new_self.real_num_data.is_some() {
-            new_self.real_num_data = Some(-new_self.real_num_data.unwrap());
-        } else if new_self.real_int_data.is_some() {
-            new_self.real_int_data = Some(-new_self.real_int_data.unwrap());
-        } else if new_self.lat_num_data.is_some() {
-            new_self.lat_num_data = Some(-new_self.lat_num_data.unwrap());
-        } else if new_self.lat_int_data.is_some() {
-            new_self.lat_int_data = Some(-new_self.lat_int_data.unwrap());
-        } else if new_self.ls_data.is_some() {
-            let mut new_ls = Vec::new();
-            let ls = new_self.ls_data.unwrap();
-            for var in ls {
-                new_ls.push(var.to_neg());
-            }
-            new_self.ls_data = Some(new_ls);
-        }
-        
-        new_self
-    }
-
-}
+use crate::{
+    var::Var,
+    parser::Token
+};
 
 #[derive(Clone, Debug)]
 pub struct Func {
@@ -198,6 +98,14 @@ fn eval_expr(ast: &Token, env: &Environment) -> Var {
                     "-" => eval_expr(exp, env).to_neg(),
                     _ => Var::impossible()
                 }
+            }
+        }, Token::ExponentialExpression(left, right) => {
+            if right.is_none() {
+                eval_expr(left, env)
+            } else {
+                let left_val = eval_expr(left, env);
+                let right_val = eval_expr(right.clone().unwrap().as_ref(), env);
+                left_val + right_val
             }
         }, _ => Var::impossible()
     }
