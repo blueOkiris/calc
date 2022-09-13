1b8c549f335655a96ed53887c72a7973b722d70d
Dylan Turner <dylantdmt@gmail.com>
2022-09-13 18:24:09 -0500

    plugin.rs: create place for executing dynamic library calls

---
+++ src/plugin.rs
@@ -0,0 +1,9 @@
+/*
+ * Author: Dylan Turner
+ * Description:
+ * - You can call external functions from .so files via the built-in 'call' function
+ * - Under the hood, it uses this code to do so
+ */
+
+
+
